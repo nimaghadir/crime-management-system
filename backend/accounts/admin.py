a32d0bd5220776487b702c7047845dfb8389ec68
Amir Kooshky <kooshkya@gmail.com>
2026-02-22 11:55:57 +0330

    create admin

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -1,3 +1,21 @@
-from django.contrib import admin
+# accounts/admin.py
 
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        ('LAPD Identification', {'fields': ('phone_number', 'national_id')}),
+    )
+    
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('LAPD Identification', {
+            'fields': ('email', 'phone_number', 'national_id'),
+        }),
+    )
+    
+    list_display = ('username', 'email', 'phone_number', 'national_id', 'is_staff', 'is_active')
+    
+    search_fields = ('username', 'email', 'phone_number', 'national_id')
