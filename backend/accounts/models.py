eabcca3b23d3769cf4b36afcffb3d9697c936bce
Amir Kooshky <kooshkya@gmail.com>
2026-02-22 11:49:17 +0330

    create user model

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,3 +1,15 @@
+# accounts/models.py
+
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=15, unique=True)
+    national_id = models.CharField(max_length=20, unique=True)
+
+    # Django's AbstractUser already includes username, password, 
+    # first_name, last_name, and relationships to Group (for roles).
+
+    def __str__(self):
+        return self.username
