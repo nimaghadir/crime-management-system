71cda4f86a7f327188640bdb571e9d764954279a
Amir Kooshky <kooshkya@gmail.com>
2026-02-22 14:43:03 +0330

    create admin panels for notifications app

--- backend/notifications/admin.py
+++ backend/notifications/admin.py
@@ -1,3 +1,12 @@
+# notifications/admin.py
 from django.contrib import admin
+from .models import Notification
 
-# Register your models here.
+
+@admin.register(Notification)
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = ('id', 'recipient', 'notif_type', 'title', 'is_read', 'created_at')
+    list_filter = ('notif_type', 'is_read')
+    search_fields = ('recipient__username', 'title', 'body')
+    raw_id_fields = ('recipient',)
+    readonly_fields = ('created_at', 'read_at')
